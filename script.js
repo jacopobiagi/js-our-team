e581916a34f794fdc886904fdd06ae5c90f896e2
jacopobiagi <jacopo.biagi02@gmail.com>
2023-12-01 15:59:46 +0100

    Inserimento oggetto js del team

---
+++ script.js
@@ -0,0 +1,42 @@
+let objecTeam = [
+    {
+        name: "Wayne Barnett",
+        role: "founder e CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        name: "Angela Carol",
+        role: "Chief Editor",
+        img: "angela-carol-chief-editor.jpg"
+    },
+
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        name: "Angela Lopez",
+        role: "Social media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+for(let i in objecTeam){
+    
+    console.log(objecTeam[i]);
+}
