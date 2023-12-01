5dacd84e520d4449fc327ab7247f5f6a368ca811
jacopobiagi <jacopo.biagi02@gmail.com>
2023-12-01 17:11:28 +0100

    Inserimento template associato a js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let objecTeam = [
     },
 
     {
-        name: "Angela Carol",
+        name: "Angela Caroll",
         role: "Chief Editor",
-        img: "angela-carol-chief-editor.jpg"
+        img: "angela-caroll-chief-editor.jpg"
     },
 
     {
@@ -36,7 +36,21 @@ let objecTeam = [
     },
 ]
 
+let padre = document.getElementsByClassName("container")[0];
+
+
 for(let i in objecTeam){
-    
-    console.log(objecTeam[i]);
+
+    let immagine = objecTeam[i].img;
+    let nome = objecTeam[i].name;
+    let ruolo = objecTeam[i].role
+
+    let figlio = document.createElement("div");
+    figlio.classList = "col-12 col-sm-6 col-md-4 d-flex flex-column justify-content-center";
+
+    figlio.innerHTML = '<img src="img/'+immagine+'" alt="team">';
+    figlio.innerHTML += '<h5>'+nome+'</h5>';
+    figlio.innerHTML += '<h6>'+ruolo+'</h6>';
+
+    padre.append(figlio);
 }
